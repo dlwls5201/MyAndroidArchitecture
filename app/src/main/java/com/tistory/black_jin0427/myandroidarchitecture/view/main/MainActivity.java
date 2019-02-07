24d56d3d6e0cb2c22d2abcd3db4218b655030be3
iwedding <dlwls5201@naver.com>
2019-02-07 12:14:11 +0900

    simple modify

--- app/src/main/java/com/tistory/black_jin0427/myandroidarchitecture/view/main/MainActivity.java
+++ app/src/main/java/com/tistory/black_jin0427/myandroidarchitecture/view/main/MainActivity.java
@@ -1,12 +1,10 @@
 package com.tistory.black_jin0427.myandroidarchitecture.view.main;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 
